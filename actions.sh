2ddfab2b4dd0c857effe494dc69d17bf82885efc
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2020-06-09 17:59:13 +0200

    fixed actions script

--- actions.sh
+++ actions.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-DIR_WITH_CHANGES=$(git diff --dirstat=files,0 HEAD~0 | sed 's/^[ 0-9.]\+% //g')
+DIR_WITH_CHANGES=$(git diff --dirstat=files,0 HEAD~1 | sed 's/^[ 0-9.]\+% //g')
 
 for DIR in ${DIR_WITH_CHANGES}; do
     if [ -d "${DIR}" ]; then
         pushd "${DIR}"
 
-        for DIR_IMAGE in *; do
-            if [ -d "$DIR_IMAGE" ]; then
-                printf "Compiling '%s' dir..." "${DIR}"
-                make login build push TAG="$DIR_IMAGE"
-            fi
-        done
-        
+        if [ -f "Makefile" ]; then
+            for DIR_IMAGE in *; do
+                if [ -d "$DIR_IMAGE" ]; then
+                    printf "Compiling '%s'..." "${DIR}/${DIR_IMAGE}"
+                    make login build push TAG="$DIR_IMAGE"
+                fi
+            done
+        fi
+
         popd
     fi
 done
